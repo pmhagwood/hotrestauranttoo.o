de028caa908c34a00ba86597603475a6d2cee21e
John Garner <jdextergarneriv@gmail.com>
2020-11-10 19:29:55 -0500

    server js added

---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 6000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var reservations = [];
+var waitlist = [];
